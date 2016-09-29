4c220bddd9ee3f3fb3989d96e3bbfe1d3077bf92
Joscha Feth <joscha@feth.com>
2016-09-30 01:12:04 +1000

    test: Use file helper for chai

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const chai = require('chai');
+const chaiFiles = require('chai-files');
+const { file } = chaiFiles;
 
 const indexifier = require('../');
 
+chai.use(chaiFiles);
 chai.should();
 
 const fixturesDir = path.join(__dirname, 'fixtures');
@@ -13,31 +16,18 @@ describe('indexifier', () => {
         const dir = path.join(fixturesDir, '1');
         it('as-is', () => {
             const ret = indexifier(dir);
-            ret.should.be.equal(
-`1
-├─┬ A
-│ └── c.html
-├── a.html
-├── a.txt
-└── b.html
-`);
+            ret.should.be.equal(file('test/fixtures/1.txt'));
         });
 
         it('filtered by extensions', () => {
             const ret = indexifier(dir, ['.html']);
-            ret.should.be.equal(
-`1
-├─┬ A
-│ └── c.html
-├── a.html
-└── b.html
-`);
+            ret.should.be.equal(file('test/fixtures/1.filtered.txt'));
         });
 
         describe('html', () => {
             it('to HTML', () => {
                 const ret = indexifier(dir, null, true);
-                ret.should.be.equal(fs.readFileSync(path.join(fixturesDir, '1.html'), 'utf8'));
+                ret.should.be.equal(file('test/fixtures/1.html'));
             });
         });
     });
