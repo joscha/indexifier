365bc1c76eb835c8d411c06210267185cfcbcf7a
Joscha Feth <joscha@feth.com>
2020-04-21 20:05:10 +1000

    fixes for PR #31 (#32)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const dirTree = require('directory-tree');
 const archy = require('archy');
 
+const { filterToMaxDepth, filterIncluded, filterEmptyDirectories } = require('./dirTreeFilters')
 const dirTreeToArchyTree = require('./dirTreeToArchyTree');
 const wrapHtml = require('./wrapHtml');
 const { DirectoryInvalidError } = require('./exceptions');
@@ -11,29 +12,23 @@ const defaultOpts = {
     fileTypes: null,
     isHtml: false,
     linkFolders: true,
+    include: undefined,
     exclude: undefined,
     emptyDirectories: true,
+    maxDepth: Infinity,
 };
-
-function filterEmptyDirectories(tree) {
-    if (tree.children && tree.children.length > 0) {
-        tree.children.forEach(child => {
-            if (child.type === 'directory') {
-                child.children = filterEmptyDirectories(child);
-            }
-        });
-    }
-    return tree.children = tree.children.filter(child => child.type === 'file' || child.children.length > 0);
-}
 
 /**
 * Generates a directory tree from the given directory and all sub-directories
-*
 * @param {!String} dir The directory to use as the start (this will be the root node of the tree)
 * @param {Object} [opts] An object which supports the following options:
 *                        {Array.<String>} fileTypes The file types to print. Defaults to all file types.
+*                        {RegExp|undefined} include A regular expression matching files/directories to include.
 *                        {RegExp|undefined} exclude A regular expression matching files/directories to exclude.
 *                        {Boolean} isHtml Whether to produce HTML output. Defaults to false.
+*                        {Boolean} linkFolders Link folders when in HTML output mode. Defaults to true.
+*                        {Boolean} emptyDirectories Include empty directories. Defaults to true.
+*                        {Number} maxDepth Limit results to a maximum sub-directory depth. Defaults to no limit.
 * @return {String} A unicode string containing a directory tree
 */
 module.exports = (dir, opts) => {
@@ -46,7 +41,7 @@ module.exports = (dir, opts) => {
     if (!stats.isDirectory()) {
         throw new DirectoryInvalidError(`Given directory "${dir}" is not valid`);
     }
-    const { exclude, fileTypes, isHtml, linkFolders, emptyDirectories } = Object.assign({}, defaultOpts, opts);
+    const { include, exclude, fileTypes, isHtml, linkFolders, emptyDirectories, maxDepth } = Object.assign({}, defaultOpts, opts);
 
     let tree = dirTree(dir, {
         exclude: exclude
@@ -57,6 +52,13 @@ module.exports = (dir, opts) => {
             : undefined,
     });
 
+    if (maxDepth !== Infinity) {
+        filterToMaxDepth(tree, maxDepth);
+    }
+    if (include) {
+        // Don't filter out the top level (cwd)
+        tree.children = tree.children.filter(child => filterIncluded(child, new RegExp(include)));
+    }
     if (!emptyDirectories) {
         filterEmptyDirectories(tree);
     }
