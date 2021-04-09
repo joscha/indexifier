dc7e93f6408d3c5972e328475fceed25138a741c
Christian Scott <christian.s@canva.com>
2021-04-09 09:07:18 +0000

    fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,39 @@
-interface Node {
-    name: string;
-    children: Node[];
-}
-
-export interface Printer {
-    print(node: Node): string;
-    printNode(node: Node): string; 
-}
-
-export type IndexifierOpts = {
-    fileTypes?: string[];
-    isHtml?: boolean;
-    linkFolders?: boolean;
-    include?: RegExp;
-    exclude?: RegExp;
-    emptyDirectories?: boolean;
-    maxDepth?: number;
-    printer?: Printer;
-};
-
-declare function indexifier(dir: string, opts?: IndexifierOpts): string;
-
-declare class AbstractPrinter {
-    print(node: Node): string;
-    abstract printNode(node: Node): string;
-}
-
-export = indexifier & { AbstractPrinter };
+declare module 'indexifier' {
+    export interface Node {
+        name: string;
+        children: Node[];
+    }
+  
+    export interface Printer {
+        print(node: Node): string;
+        printNode(node: Node): string;
+    }
+  
+    export type IndexifierOpts = {
+        fileTypes?: string[];
+        isHtml?: boolean;
+        linkFolders?: boolean;
+        include?: RegExp;
+        exclude?: RegExp;
+        emptyDirectories?: boolean;
+        maxDepth?: number;
+        printer?: Printer;
+    };
+  
+    declare function indexifier(dir: string, opts?: IndexifierOpts): string;
+  
+    declare class AbstractPrinter {
+        print(node: Node): string;
+        abstract printNode(node: Node): string;
+    }
+  
+    interface Indexifier {
+      (dir: string, opts?: IndexifierOpts): string;
+      AbstractPrinter: typeof AbstractPrinter;
+    }
+  
+    declare const exports: Indexifier;
+  
+    export = exports;
+  }
+  
