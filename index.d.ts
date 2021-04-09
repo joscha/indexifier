70cebcfe36b6d3869daf7f09864f7c990973c284
Christian Scott <christian.s@canva.com>
2021-04-09 08:28:26 +0000

    add type definitions

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+interface Node {
+    name: string;
+    children: Node[];
+}
+
+export interface Printer {
+    print(node: Node): string;
+    printNode(node: Node): string; 
+}
+
+export type IndexifierOpts = {
+    fileTypes?: string[];
+    isHtml?: boolean;
+    linkFolders?: boolean;
+    include?: RegExp;
+    exclude?: RegExp;
+    emptyDirectories?: boolean;
+    maxDepth?: number;
+    printer?: Printer;
+};
+
+declare function indexifier(dir: string, opts?: IndexifierOpts): string;
+
+declare class AbstractPrinter {
+    print(node: Node): string;
+    abstract printNode(node: Node): string;
+}
+
+export = indexifier & { AbstractPrinter };
